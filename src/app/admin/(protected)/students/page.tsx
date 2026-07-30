4cf177c2a2baaf75fdf349b8a1ef213113e8e85f
German Abrikosov <etogerman@gmail.com>
2026-07-31 02:41:09 +0300

    Убрать избыточное описание списка учеников

--- src/app/admin/(protected)/students/page.tsx
+++ src/app/admin/(protected)/students/page.tsx
@@ -131,11 +131,6 @@ export default async function AdminStudentsPage({
       <header className="admin-page-heading admin-page-heading-wide">
         <p className="overline">Поддержка учеников</p>
         <h1>Ученики</h1>
-        <p>
-          Поиск по учётным данным и просмотр фактического
-          оплаченного доступа. Управление активными сессиями
-          доступно в карточке ученика.
-        </p>
       </header>
 
       <form
